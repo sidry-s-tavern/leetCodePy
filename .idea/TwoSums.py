51a5680feb45462c02a73b89999ffb54f5f02dc6
sidry-s-tavern <isgudoshnikov@gmail.com>
2025-02-20 09:35:35 +0400

    Two Sums. Some a little code

---
+++ .idea/TwoSums.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: [int], target: int) -> [int]:
+        nums = [1, 2, 3]
+        target = 6
+        print(nums, target)
+        return nums
+
+print(Solution.twoSum(Solution, [1, 2, 3], 6))
