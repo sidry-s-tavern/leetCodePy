5d1c508804b43579623186fa1aa85141d6c4392d
sidry-s-tavern <isgudoshnikov@gmail.com>
2025-02-21 13:05:15 +0400

    Palindrome Number. Init commit

--- .idea/PalindromeNumber.py
+++ .idea/PalindromeNumber.py
@@ -1,3 +1,12 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        for i in range(len(x+"")):
+            print((x+"")[i])
         return True
+
+Solution.isPalindrome(123)
+        # String xString = Integer.toString(x);
+        # for (int i = 0; i < xString.length(); i++) {
+        # if(xString.charAt(i) != xString.charAt(xString.length()-i-1)) return false;
+        # }
+        # return true;
