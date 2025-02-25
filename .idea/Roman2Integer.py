6799f9f54b094357ee219ee24624e4d38aa99f09
sidry-s-tavern <isgudoshnikov@gmail.com>
2025-02-26 00:17:33 +0400

    Roman2Int. done, but without match-case

--- .idea/Roman2Integer.py
+++ .idea/Roman2Integer.py
@@ -1,35 +1,36 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        Solution.romanToIntChar(Solution,'I')
+        res = 0
+        con = False
+        for i in range(len(s)):
+            if con:
+                con = False
+                continue
+            curr = Solution.romanToIntChar(s[i])
+            print("i", i, "curr", curr)
+            next = 0
+            if i != (len(s) - 1):
+                next = Solution.romanToIntChar(s[i + 1])
+                print("next", next)
+                if curr < next:
+                    res = res + next - curr
+                    con = True
+                else:
+                    res += curr
+            else:
+                res += curr
+            print("res", res)
+        return res
+
+    def romanToIntChar(s: str) -> int:
+        if s == 'I': return 1
+        if s == 'V': return 5
+        if s == 'X': return 10
+        if s == 'L': return 50
+        if s == 'C': return 100
+        if s == 'D': return 500
+        if s == 'M': return 1000
         return -1
 
-    def romanToIntChar(self, s: str) -> int:
-        match s:
-            case 'I': return 1
-            case 'V': return 5
-            case 'X': return 10;
-            case 'L': return 50;
-            case 'C': return 100;
-            case 'D': return 500;
-            case 'M': return 1000;
-            case _: return -1
-        return -1
-#   public int romanToInt(String s) {
-#       int res = 0;
-#       for (int i = 0; i < s.length(); i++) {
-#           int curr = romanToIntChar(s.charAt(i));
-#           int next = 0;
-#           if (i != s.length()-1) {
-#               next = romanToIntChar(s.charAt(i + 1));
-#               if (curr < next) {
-#                   res = res + next - curr;
-#                   i++;
-#               } else {
-#                   res += curr;
-#               }
-#           } else {
-#               res += curr;
-#           }
-#       }
-#       return res;
-#   }
+
+print(Solution.romanToInt(Solution, "MCMXCIV"))
