d754b4ea0a89f200df97a72fbfdfcf3edbd34308
sidry-s-tavern <isgudoshnikov@gmail.com>
2026-02-12 13:54:39 +0400

    isBadVersion 11 / 24 testcases passed

---
+++ .idea/IsBadVersion.py
@@ -0,0 +1,14 @@
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        if Solution.isBadVersion(n):
+            print(f"{n}->{n-1}")
+            return Solution.firstBadVersion(Solution, n - 1)
+        return n+1
+
+    def isBadVersion(version: int) -> bool:
+        if version <= 4:
+            return False
+        return True
+
+
+print(Solution.firstBadVersion(Solution, 6))
