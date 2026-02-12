247fd2dfe6a6e0c3b7fcce792ace4de81587e985
sidry-s-tavern <isgudoshnikov@gmail.com>
2026-02-12 14:46:40 +0400

    BetsTime2Buy'n'Sell almost done

---
+++ .idea/BestTime2Buy'n'SellStock.py
@@ -0,0 +1,13 @@
+class Solution:
+    """
+    Time Limit Exceeded 200 / 212 testcases passed
+    """
+    def maxProfit(self, prices: list[int]) -> int:
+        profit = 0
+        for i in range(len(prices) - 1):
+            if profit < max(prices[i + 1:]) - prices[i]:
+                profit = max(prices[i + 1:]) - prices[i]
+        return profit
+
+
+print(Solution.maxProfit(Solution, [7, 1, 5, 3, 6, 4]))
--- .idea/IsBadVersion.py
+++ .idea/IsBadVersion.py
@@ -1,4 +1,7 @@
 class Solution:
+    """
+    is not done: 11 / 24 Time Limit Exceeded
+    """
     def firstBadVersion(self, n: int) -> int:
         if Solution.isBadVersion(n):
             print(f"{n}->{n-1}")
